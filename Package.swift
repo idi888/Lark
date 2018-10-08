ef47f00019c1528bd1576415344f6827457b9b15
Adir @ WorkOnMac <adir@hanave.com>
2018-10-09 09:46:47 +1100

    :P' git commit -m :P" "

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Lark",
+    name: "ALark",
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.5.0")
     ],
     targets: [
-        .target(name: "Lark", dependencies: ["Alamofire"]),
+        .target(name: "ALark", dependencies: ["Alamofire"]),
         .target(name: "CodeGenerator", dependencies: ["Lark", "SchemaParser"]),
         .target(name: "SchemaParser", dependencies: ["Lark"]),
         .target(name: "lark-generate-client", dependencies: ["SchemaParser", "CodeGenerator"])
